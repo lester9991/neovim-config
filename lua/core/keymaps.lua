43a3f34968bfac92fb1622cda7754e8aced0b605
lester9991 <lester@lattu.localdomain>
2026-02-04 04:17:03 +0000

    update keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -132,3 +132,13 @@ keymap.set("t",'<C-q>','exit<CR>')
 
 keymap.set("n","j","jzz")
 keymap.set("n","k","kzz")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
